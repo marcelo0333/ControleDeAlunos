a0953d7b33ebeb7ba4173e9703b72bcc00939738
marcelo0333 <139006301+marcelo0333@users.noreply.github.com>
2026-02-23 10:33:07 -0300

    feat: adicionado suporte a CORS para permitir requisições de origens específicas

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import router from './index.js';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+
 app.use(express.json());
 app.use('/api', router);
+
 
 export default app;
